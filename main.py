7a5e15bc8af35f545b439851e9c6dcf998449fa9
nareenz125-creator <nareenz125@gmail.com>
2026-01-05 16:22:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ model = Pipeline([
 ])
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 42)
+model.fit(x, y)
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression(max_iter = 99)
 model.fit(x_train, y_train)
