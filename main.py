889db73f31153fe0452b5ce5b87afa97b42b1d5f
nareenz125-creator <nareenz125@gmail.com>
2026-01-05 16:27:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
-!wget htt!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
 !wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
 
 import lab_setup_do_not_edit
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LogisticRegression
-import joblib
 
-# قراءة الداتا
 df = pd.read_csv('parkinsons.csv')
 df = df.dropna()
+df.head()
 print(df.columns.to_list())
 
-# Optional visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
 sns.pairplot(df, hue='status', diag_kind='kde', corner=True)
 plt.show()
 
-# اختيار الميزتين
 selected_features = ['MDVP:Flo(Hz)', 'MDVP:Jitter(%)']
 x = df[selected_features]
 y = df['status']
 
-# انشاء pipeline مع scaler
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+
 model = Pipeline([
     ('scaler', MinMaxScaler()),
     ('logreg', LogisticRegression(max_iter=1000))
 ])
 
-# تدريب على كل البيانات
 model.fit(x, y)
 
-# حفظ الموديل
+import joblib
 joblib.dump(model, 'my_model.joblib')
+
