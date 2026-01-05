a01fdb8330dd5416627745435bd2845284b4e72d
nareenz125-creator <nareenz125@gmail.com>
2026-01-05 16:25:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget htt!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
 !wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+
 import lab_setup_do_not_edit
 import pandas as pd
-df = pd.read_csv('parkinsons.csv')
-df = df.dropna()
-df.head()
-print(df.columns.to_list())
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.pairplot(df, hue='status', diag_kind='kde', corner=True)
-plt.show()
-selected_features = ['MDVP:Flo(Hz)', 'MDVP:Jitter(%)']
-x = df[selected_features]
-y = df['status']
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
+import joblib
 
+# قراءة الداتا
+df = pd.read_csv('parkinsons.csv')
+df = df.dropna()
+print(df.columns.to_list())
+
+# Optional visualization
+sns.pairplot(df, hue='status', diag_kind='kde', corner=True)
+plt.show()
+
+# اختيار الميزتين
+selected_features = ['MDVP:Flo(Hz)', 'MDVP:Jitter(%)']
+x = df[selected_features]
+y = df['status']
+
+# انشاء pipeline مع scaler
 model = Pipeline([
     ('scaler', MinMaxScaler()),
     ('logreg', LogisticRegression(max_iter=1000))
 ])
 
-from sklearn.model_selection import train_test_split
+# تدريب على كل البيانات
 model.fit(x, y)
-from sklearn.linear_model import LogisticRegression
-model = LogisticRegression(max_iter = 99)
-model.fit(x_train, y_train)
-from sklearn.metrics import accuracy_score
-y_pred = model.predict(x_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
-import joblib
 
+# حفظ الموديل
 joblib.dump(model, 'my_model.joblib')
