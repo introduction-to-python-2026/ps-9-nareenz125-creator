6702aa2bc0cde3de4417778b246db0c0e8fbbfdf
nareenz125-creator <nareenz125@gmail.com>
2026-01-06 21:04:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,3 +31,7 @@ from sklearn.metrics import accuracy_score
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
+
+import joblib
+
+joblib.dump(RandomForestClassifier, 'my_model.joblib')
