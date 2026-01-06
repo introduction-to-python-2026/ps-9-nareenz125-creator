240c8deb693d1e645888b3238630dd30ee572872
nareenz125-creator <nareenz125@gmail.com>
2026-01-06 21:49:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Download the data from your GitHub repository
 !wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
 !wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
 import lab_setup_do_not_edit
@@ -30,5 +31,5 @@ accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
 
 import joblib 
-joblib.dump((model, scaler), 'my_model.joblib')
 
+joblib.dump(RandomForestClassifier, 'my_model.joblib')
