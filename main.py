12b8dff78071155345c66c64c65ceb27456a428a
nareenz125-creator <nareenz125@gmail.com>
2026-01-08 17:28:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
-import lab_setup_do_not_edit
-
 import pandas as pd
-df = pd.read_csv('/content/parkinsons.csv')
-df.head()
+df = pd.read_csv("/content/parkinsons.csv")
+df = df.dropna()
 
 print(df.columns.to_list())
-
-selected_features = df[['MDVP:Fo(Hz)', 'MDVP:Flo(Hz)']] 
-output = df['status']
-x = selected_features
-y = output 
+selected_features = ['RPDE', 'PPE']
+x = df[selected_features]
+y = df['status']
 
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 x = scaler.fit_transform(x)
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.6, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier()
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier(max_depth = 3)
 model.fit(x_train, y_train)
 
 from sklearn.metrics import accuracy_score
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
-
-import joblib 
-
-joblib.dump(model, 'my_model.joblib')
